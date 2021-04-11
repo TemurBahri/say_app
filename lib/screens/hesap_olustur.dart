280b6de2c511a3692ae67b71c70013b8828e29ba
Temur <77517397+TemurBahri@users.noreply.github.com>
2021-04-11 19:17:18 +0300

    11.04.2021

---
+++ lib/screens/hesap_olustur.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HesapOlustur extends StatefulWidget {
+  @override
+  _HesapOlusturState createState() => _HesapOlusturState();
+}
+
+class _HesapOlusturState extends State<HesapOlustur> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Hesap Oluştur")),
+    );
+  }
+}
